63308a7ff41a666bf496b9305b61886dd3d1a89b
Avgeya India <159917433+github-avegya@users.noreply.github.com>
2024-10-03 05:15:01 +0530

    chore: fixed deployment error

--- schema/image.model.js
+++
@@ -1,8 +0,0 @@
-import { Schema, model, models } from "mongoose";
-
-const ImageSchema = new Schema({
-    secure_url: {type: String},
-    public_id: {type: String}, 
-}, {timestamps: true})
-
-export const Image = models?.Image || model('Image', ImageSchema)
